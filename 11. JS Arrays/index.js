2f3856b60745c99dfab98e2f23ef4e2ea7b3cbb2
Leandro Bellini <leandroluisbellini@gmail.com>
2017-06-20 21:35:48 -0300

    JS functions

--- 11. JS Arrays/index.js
+++ 11. JS Arrays/index.js
@@ -25,3 +25,40 @@ console.log(part);
 
 var two = [["Leandro", "Fernando"],["Bellini", "Pereira"]];
 alert(two[0][1] + " " + two[1][1]);
+
+amigos.forEach(function(c){
+  console.log(c);
+});
+
+//vê se todos elementos do array sao iguais! Se sim... retorna true
+function isUniform(vetor){
+   	var primeiro = vetor[0];
+    var teste = true;
+    vetor.forEach(function(elemento){
+        if(elemento !== primeiro){
+            teste =  false;
+        }
+    })
+    return teste;
+};
+
+//retorna a soma dos elementos do vetor
+function soma(vetor){
+	var sum = 0;
+    vetor.forEach(function(x){
+        sum += x;
+    });
+	return sum;
+};
+
+
+
+//retorna o maior valor de um array
+function max(vetor){
+    var num = vetor[0];
+    vetor.forEach(function(x){
+        if(x > num)
+            num = x;
+    });
+    console.log("Maior valor: " + num);
+};
