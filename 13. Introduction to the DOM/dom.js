5c9708796d7f3a74b2b859ddb1abe97f9dd0ff53
Leandro Bellini <leandroluisbellini@gmail.com>
2017-06-21 22:25:25 -0300

    DOM Advanced

--- 13. Introduction to the DOM/dom.js
+++ 13. Introduction to the DOM/dom.js
@@ -2,6 +2,10 @@
 var h1 = document.querySelector("h1");
 var isBlue = false;
 
+console.log(h1.textContent);
+//tag.textContent
+//h1.innerHTML
+
 setInterval(function(){
   if(isBlue){
     h1.style.color = "red";
@@ -13,6 +17,17 @@ setInterval(function(){
 
 }, 500);
 
+
+var link = document.querySelector("a");
+var l = link.getAttribute("href");
+
+console.log("Link ->  " + l);
+
+link.setAttribute("href", "http://fb.com");
+
+l = link.getAttribute("href");
+
+console.log("Link ->  " + l);
 
 /*
 document.getElementById(  );
@@ -33,3 +48,14 @@ el = document.getElementById("elemento");
 console.log(typeof(el));
 
 console.log(el);
+
+//add and remove class
+el.classList.remove("classe");
+el.classList.add("outra classe");
+
+el.classList.toggle("claase"); //remove se jah existir a classe... add se nao tiver!
+
+el.textContent
+
+
+//
