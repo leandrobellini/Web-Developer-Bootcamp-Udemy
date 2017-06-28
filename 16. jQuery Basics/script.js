207b4a8adeaa1f35b5d5b03af77321b7a755a68d
Leandro Bellini <leandroluisbellini@gmail.com>
2017-06-28 20:41:28 -0300

    More jQuery

---
+++ 16. jQuery Basics/script.js
@@ -0,0 +1,16 @@
+var myStyle = {
+  color : "red",
+  border: "2px solid purple"
+}
+
+$("h1").css(myStyle);
+
+$("li").css({
+  color: "red",
+  background: "blue"
+});
+
+$("div").css("background","purple");
+$(".highlight").css("width", "200px");
+$("#third").css("border","2px solid orange");
+$("div:first-of-type").css("color","pink");
