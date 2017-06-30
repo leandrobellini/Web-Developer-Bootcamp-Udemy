b16b97e7e3715f72efbd7760e0fc9f4293fc6d7f
Leandro Bellini <leandroluisbellini@gmail.com>
2017-06-29 22:32:56 -0300

    To-Do Project

--- 18. Todo List Project/assets/js/todos.js
+++ 18. Todo List Project/assets/js/todos.js
@@ -1,12 +1,20 @@
 
-$("li").on("click", function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("done");
 });
 
-$("span").on("click", function(event){
+$("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
 
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    var conteudo = $(this).val();
+    $("ul").append("<li><span>X </span>" + conteudo +"</li>");
+    $(this).val("");
+  }
 });
